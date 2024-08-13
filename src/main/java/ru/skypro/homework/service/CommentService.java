c57ef87ff7ddc8d5928c31822fa90d8b6956c525
Lyamzin <lyamzin01@mail.ru>
2024-08-13 16:47:24 +1000

    Добавил комментарии (сервис,конструктор,сущность) Изменил User,Login ,Register

---
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -0,0 +1,41 @@
+package ru.skypro.homework.service;
+
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.Comment;
+import ru.skypro.homework.repository.CommentRepository;
+
+import java.util.Collection;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+public Comment addComment(Comment comment) {
+        return commentRepository.save(comment);
+}
+
+public Comment editComment(Comment comment) {
+        return commentRepository.save(comment);
+}
+public void deleteComment(long id) {
+        commentRepository.deleteById(id);
+}
+public Collection<Comment> getAllComments() {
+        return commentRepository.findAll();
+}
+public Comment findCommentById(long id) {
+        return commentRepository.findByCommentId(id);
+}
+public Comment updateComment (Comment comment) {
+        return commentRepository.save(comment);
+}
+
+}
