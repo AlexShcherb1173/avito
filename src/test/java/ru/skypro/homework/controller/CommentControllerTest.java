a4f002ab3138b0716d6cf13b0e94a3036cd8d9e9
bogomolova <ibogomolllova@gmail.com>
2025-03-30 14:51:03 +0300

    fix: CommentControllerTest

--- src/test/java/ru/skypro/homework/controller/CommentControllerTest.java
+++ src/test/java/ru/skypro/homework/controller/CommentControllerTest.java
@@ -14,13 +14,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.skypro.homework.dto.CommentDTO;
 import ru.skypro.homework.dto.CommentsDTO;
 import ru.skypro.homework.dto.CreateOrUpdateCommentDTO;
+import ru.skypro.homework.exception.CommentNotFoundException;
 import ru.skypro.homework.service.CommentService;
 import ru.skypro.homework.service.security.CommentSecurityService;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,7 +42,7 @@ public class CommentControllerTest {
 
     @Test
     @WithMockUser
-    public void shouldReturnComments() throws Exception {         // проходит
+    public void shouldReturnComments() throws Exception {
         when(commentService.getComments(anyInt())).thenReturn(new CommentsDTO());
 
         mockMvc.perform(MockMvcRequestBuilders.get("/ads/1/comments"))
@@ -51,21 +51,39 @@ public class CommentControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "USER")                                  // не проходит
+    @WithMockUser(roles = "USER")
     public void shouldAddComment() throws Exception {
         CommentDTO commentDTO = new CommentDTO();
         when(commentService.addComment(anyInt(), any())).thenReturn(commentDTO);
+        when(commentSecurityService.isOwner(anyInt())).thenReturn(false);
+
+        CreateOrUpdateCommentDTO commentRequest = new CreateOrUpdateCommentDTO();
+        commentRequest.setText("Test comment");
 
         mockMvc.perform(MockMvcRequestBuilders.post("/ads/1/comments")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(new CreateOrUpdateCommentDTO())))
-                .andExpect(status().isForbidden())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                        .content(objectMapper.writeValueAsString(commentRequest)))
+                .andExpect(status().isForbidden());
+
+        verify(commentService, never()).addComment(anyInt(), any());
     }
 
     @Test
     @WithMockUser(roles = "USER")
-    public void shouldDeleteComment() throws Exception {            // проходит
+    public void shouldNotAddCommentWhenTextIsEmpty() throws Exception {
+        CreateOrUpdateCommentDTO commentRequest = new CreateOrUpdateCommentDTO();
+        commentRequest.setText("");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/ads/1/comments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(commentRequest)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void shouldDeleteComment() throws Exception {
+        when(commentSecurityService.isOwner(anyInt())).thenReturn(true);
         doNothing().when(commentService).deleteComment(anyInt(), anyInt());
 
         mockMvc.perform(MockMvcRequestBuilders.delete("/ads/1/comments/1"))
@@ -74,14 +92,66 @@ public class CommentControllerTest {
 
     @Test
     @WithMockUser(roles = "USER")
-    public void shouldUpdateComment() throws Exception {            // не проходит
+    public void shouldNotDeleteCommentIfUnauthorized() throws Exception {
+        when(commentSecurityService.isOwner(anyInt())).thenReturn(false);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/ads/1/comments/1"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void shouldUpdateComment() throws Exception {
+        CommentDTO updatedComment = new CommentDTO();
         when(commentSecurityService.isOwner(anyInt())).thenReturn(true);
-        when(commentService.updateComment(anyInt(), anyInt(), any())).thenReturn(new CommentDTO());
+        when(commentService.updateComment(anyInt(), anyInt(), any())).thenReturn(updatedComment);
+
+        CreateOrUpdateCommentDTO updateRequest = new CreateOrUpdateCommentDTO();
+        updateRequest.setText("Updated text");
 
         mockMvc.perform(MockMvcRequestBuilders.patch("/ads/1/comments/1")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(new CreateOrUpdateCommentDTO())))
-                .andExpect(status().isForbidden())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void shouldNotUpdateCommentWhenTextIsEmpty() throws Exception {
+        CreateOrUpdateCommentDTO updateRequest = new CreateOrUpdateCommentDTO();
+        updateRequest.setText("");
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/ads/1/comments/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void shouldNotUpdateCommentIfUnauthorized() throws Exception {
+        when(commentSecurityService.isOwner(anyInt())).thenReturn(false);
+
+        CreateOrUpdateCommentDTO updateRequest = new CreateOrUpdateCommentDTO();
+        updateRequest.setText("Updated text");
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/ads/1/comments/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void shouldReturnNotFoundForNonExistingComment() throws Exception {
+        when(commentService.updateComment(anyInt(), anyInt(), any())).thenThrow(new CommentNotFoundException(1));
+
+        CreateOrUpdateCommentDTO updateRequest = new CreateOrUpdateCommentDTO();
+        updateRequest.setText("Updated text");
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/ads/1/comments/999")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andExpect(status().isForbidden());
     }
 }
