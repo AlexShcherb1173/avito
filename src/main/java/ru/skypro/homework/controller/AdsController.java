66ea4f71cdedbbee8be0e96ea80163d7fb4927ca
kirillKrivobokov <Deuse.sec@gmail.com>
2025-06-17 21:30:21 +0300

    Add_Dto_ControllerIsEmpty

---
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -0,0 +1,66 @@
+package ru.skypro.homework.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.Ads;
+
+@RestController
+@RequestMapping("/ads")
+public class AdsController {
+
+    @GetMapping
+    public ResponseEntity<?> getAllAds(@RequestBody Ads ads) {
+        return new ResponseEntity<>(ads, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addAds(@RequestBody Ads ads) {
+        return new ResponseEntity<>(ads, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getAdsById(@PathVariable("id") int id) {
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteAds(@PathVariable("id") int id) {
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<?> updateAds(@PathVariable("id") int id) {
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @GetMapping("me")
+    public ResponseEntity<?> getAdsUser() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PatchMapping("{id}/image")
+    public ResponseEntity<?> updateAdsImage(@PathVariable("id") int id) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}/comments")
+    public ResponseEntity<?> getAdsComments(@PathVariable("id") int id) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PostMapping("{id}/comments")
+    public ResponseEntity<?> addAdsComment(@PathVariable("id") int id) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{adId}/comments/{commentId}")
+    public ResponseEntity<?> deleteAdsComment(@PathVariable("adId") int adId, @PathVariable("commentId") int commentId) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PatchMapping("{adId}/comments/{commentId}")
+    public ResponseEntity<?> updateAdsComment(@PathVariable("adId") int adId, @PathVariable("commentId") int commentId) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+}
